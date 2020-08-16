c055704cd8fe4bec809e8b35f457d4d63a4fc1dc
kryptocodes <srivatsantb@gmail.com>
2020-08-16 19:43:18 +0530

    Removed pdf
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,13 +4,13 @@ import ModalBox from '../Common/modal'
 
 const Card = () => {
     return (
-        <div className="row mx-auto mt-2 d-flex justify-content-center">
+        <div className="row container mx-auto mt-2 pb-5 d-flex justify-content-around">
         {Data && Data.map((dept,index) => (
-        <div key={index} className="col-lg-2 mt-5 mr-2">
+        <div key={index} className="col-md-3 mt-5 mr-2 rounded">
         <div className="card">
             <span className="card-body">
             <strong>Year:<p className="lead">{dept.year}</p></strong>
-            <p className="text-center">{dept.dept}</p>
+            <p style={{fontFamily: "Short Stack", fontStyle: "cursive", fontSize:"5vh"}} className="text-center">{dept.dept}</p>
             </span>
             <span className="card-footer d-flex justify-content-around">
                 {ModalBox({File:dept.url})}
