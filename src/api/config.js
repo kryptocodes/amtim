14debc92a6f50814af702d2694f6db9cdd78069b
kryptocodes <srivatsantb@gmail.com>
2020-08-17 20:33:56 +0530

    Modified config
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/api/config.js
+++ src/api/config.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey: process.env.GATSBY_apiKey,
+    authDomain: process.env.GATSBY_authDomain,
+    databaseURL: process.env.GATSBY_databaseURL,
+    projectId: process.env.GATSBY_projectId,
+    storageBucket: process.env.GATSBY_storageBucket,
+    messagingSenderId: process.env.GATSBY_messagingSenderId,
+    appId: process.env.GATSBY_appId,
+    measurementId: process.env.GATSBY_measurementId
 };
 
 firebase.initializeApp(firebaseConfig);
