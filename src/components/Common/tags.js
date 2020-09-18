e00fd59bc7fd67a3b3eef1baebe4e0be58d718a5
kryptocodes <srivatsantb@gmail.com>
2020-09-18 20:37:32 +0530

    Modded page
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/components/Common/tags.js
+++ src/components/Common/tags.js
@@ -6,7 +6,9 @@ const Tags = () => {
       <div className="application">
         <Helmet>
           <meta charSet="utf-8" />
+          <html lang="en"/>
           <title>AmTim</title>
+          <link rel="canonical" href="https://amtim.netlify.app/" />
           <meta name="description" content="AmTim is an Simple PWA app to view Timetable"/>
           <meta name="keywords" content="amTim"/>
           <meta name="author" content="srivatsan"/>
