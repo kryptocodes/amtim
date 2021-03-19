705e017de94fe2b33caf2af4b74bb63b0214693f
kryptocodes <srivatsantb@gmail.com>
2021-03-19 13:54:54 +0530

    Added PWA
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const withPWA = require('next-pwa')
  
 module.exports = withPWA({
-    pwa: {
-        dest: 'public'
-    }
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
+  }
 })
