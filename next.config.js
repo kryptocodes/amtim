3d69c9cdcfd7ecfe6e92c5ddbe0c275eb77cb140
kryptocodes <srivatsantb@gmail.com>
2021-03-19 14:10:37 +0530

    added next offline
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,32 @@
-const withPWA = require('next-pwa')
- 
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    scope: '/app',
-    sw: 'service-worker.js',
-  }
-})
+const withOffline = require('next-offline')
+
+const nextConfig = {
+    workboxOpts: {
+        cleanupOutdatedCaches: true,
+        maximumFileSizeToCacheInBytes: 5000000,
+        runtimeCaching: [
+            {
+                urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+                handler: 'CacheFirst',
+                options: {
+                    cacheName: 'images',
+                    expiration: {
+                        maxEntries: 10,
+                    },
+                },
+            },
+            {
+                urlPattern: /^https?.*/,
+                handler: 'NetworkFirst',
+                options: {
+                    cacheName: 'offlineCache',
+                    expiration: {
+                        maxEntries: 200,
+                    },
+                },
+            },
+        ],
+    }
+}
+
+module.exports = withOffline(nextConfig)
